ecb5b1f8e8ecbb66c3cc24a8ac0958963c49012e
Andrzej Bobak <a.bobak@interia.pl>
2021-04-11 22:36:21 +0200

    Add entity and mapper entity mappings

--- src/main/java/com/sample/datawarehouse/mapper/TrafficDataMapper.java
+++ src/main/java/com/sample/datawarehouse/mapper/TrafficDataMapper.java
@@ -1,7 +1,15 @@
 package com.sample.datawarehouse.mapper;
 
+import com.sample.datawarehouse.dto.TrafficDataDto;
+import com.sample.datawarehouse.model.TrafficData;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TrafficDataMapper {
+
+    TrafficData dtoToEntity(TrafficDataDto dto);
+
+    List<TrafficData> dtoListToEntityList(List<TrafficDataDto> dtos);
 }
---
+++ src/main/java/com/sample/datawarehouse/model/TrafficData.java
@@ -0,0 +1,39 @@
+package com.sample.datawarehouse.model;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrafficData {
+
+    @Id
+    private Long id;
+
+    private String dataSource;
+
+    private String campaign;
+
+    private LocalDate daily;
+
+    private Integer clicks;
+
+    private Integer impressions;
+
+}
